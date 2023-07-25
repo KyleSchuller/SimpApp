433619a0cf6cdc872f752ef9e3a409fb2e469370
Kyle Schuller <33623211+KyleSchuller@users.noreply.github.com>
2023-07-25 19:32:20 +0100

    Electron Forge + WebPack + React + FontSource

---
+++ App/src/app.jsx
@@ -0,0 +1,8 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <h2>Hello from React!</h2>
+  </StrictMode>
+);
